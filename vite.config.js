7744bc68a64ce2b1f10c856ddbe73f5e6b522a37
Nuoda Yang <nuoda.yang@gmail.com>
2023-12-08 23:22:33 +1100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-export default {
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+
+export default defineConfig ({
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        maincn: resolve(__dirname, 'indexcn.html'),
+      },
+    },
   }
-}
+})
